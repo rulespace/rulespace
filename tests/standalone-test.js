e82d8d7276cbdfdc7c82da05749f77cac0771b83
eljenso <jens.nicolay@gmail.com>
2021-09-07 21:25:30 +0200

    intiial infra for lambdas/closures

--- tests/standalone-test.js
+++ tests/standalone-test.js
@@ -1,24 +1,23 @@
-import { compileToModule, sanityCheck, compileModuleTuples } from './test-common.js';
+import { compileToModule, sanityCheck } from './test-common.js';
 
 const src = `
 
-(rule [Reachable x y] [Link x y])
-(rule [Reachable x y] [Reachable x z] [Link z y])
-
+(rule [F f] (:= x 123) (:= f (lambda () x)))
+(rule [R x] [F f] (:= x (f)))
 `;
 
 
 compileToModule(src, 'standalone', {debug:true, assertions:true}).then(module => {
 //import('./compiled/standalone.mjs').then(module => {
 
-module.addTuples(compileModuleTuples(module, `[Link "a" "b"] [Link "b" "c"]`));
+// module.addTuples(compileModuleTuples(module, ``));
 console.log("tuples: " + [...module.tuples()].join('\n'));
 console.log("roots: " + [...module.rootTuples()].join('\n'));
 sanityCheck(module);
 
-console.log("\n\n\n");
-module.removeTuples(compileModuleTuples(module, `[Link "a" "b"]`).map(t => t.get())); 
-console.log("tuples: " + [...module.tuples()].join('\n'));
-sanityCheck(module);
+// console.log("\n\n\n");
+// module.removeTuples(compileModuleTuples(module, `[Link "a" "b"]`).map(t => t.get())); 
+// console.log("tuples: " + [...module.tuples()].join('\n'));
+// sanityCheck(module);
 })
 
